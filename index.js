84aa670bcf64df3f1e6d121c0748aae6563b9e51
Gabriel Carneiro Jr <gacarneirojr@hotmail.com>
2022-11-07 14:53:11 -0300

    working solution. Time format pending

--- index.js
+++ index.js
@@ -2,22 +2,37 @@ const { getVideoDurationInSeconds } = require('get-video-duration')
 const path = require('path');
 const fs = require('fs');
 
+class Videos {
 
-(async () => {
-const directoryPath = path.join(__dirname, 'videos');
+    createFileVideosDuration = async () => {
+        const directoryPath = path.join(__dirname, 'videos');
 
-global.videos = []
+        let videos = ""
+        const files = fs.readdirSync(directoryPath)
 
-const files = fs.readdirSync(directoryPath)
+        for (const file of files) {
+                videos += file +" " + (await getVideoDurationInSeconds(`videos/${file}`)).toFixed(0)+"\r\n"
+        }
+        this._saveInFile(videos)
+        }
+    
+    _saveInFile = (videos) => {
+            fs.writeFileSync('videos.txt', videos, function (err) {
+                if (err) return console.log(err);
+                console.log(`Video ${video} added`);
+    })}
 
-for (const file of files) {
-        videos.push(
-            {
-                files: (await getVideoDurationInSeconds(`videos/${file}`)).toFixed(0)
-            })
+    _formatTime = (seconds) => {
+        const inSeconds = seconds
+
+        const minutesFraction = seconds/60
+
+        console.log(minutesFraction)
+    }
+
 
 }
 
-console.log(videos)
-})()
+const videos = new Videos()
 
+videos.createFileVideosDuration()
