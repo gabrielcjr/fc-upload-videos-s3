7e879701e293e88698e5d560a2aae6b45a2bde3c
Gabriel Carneiro Jr <gacarneirojr@hotmail.com>
2022-11-09 11:30:14 -0300

    successfully uploading to s3

--- aws.js
+++ aws.js
@@ -1,24 +1,33 @@
 require('dotenv').config()
 const AWS =require('aws-sdk');
 const fs = require('fs');
+const stream = require('stream');
+
+AWS.config.update({region: 'us-east-1'});
 
 const s3 = new AWS.S3({
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
   })
 
-const uploadFile = (file) => {
-  const fileStream = fs.createReadStream(file.path)
+const uploadFile = (file, chapter, path) => {
+  // const fileContent  = fs.readFileSync(__dirname, file)
   
+  const fileStream = new stream.PassThrough();
+  
+  const pathToS3 = path+chapter+file
+
   const uploadParams = {
-    Bucket: process.env.AWS_BUCKET_NAME,
+    Bucket: process.env.AWS_BUCKET_NAME_UPLOAD,
     Body: fileStream,
-    Key: file.filename
+    Key: pathToS3
   }
-  return s3.upload(uploadParams).promise()
+  s3.upload(uploadParams).promise()
+
+  return fileStream
 }
 
+const readStream = fs.createReadStream('videos/01-Sobre-sistema-de-migracoes-no-Sequelize.mp4');
 
-
-
+readStream.pipe(uploadFile('01-Sobre-sistema-de-migracoes-no-Sequelize.mp4','5/', process.env.REPO_TYPESCRIPT))
 
