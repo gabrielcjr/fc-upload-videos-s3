f1655fa476141c9f6e3e4eaba0da4f8103224672
Gabriel Carneiro Jr <gacarneirojr@hotmail.com>
2022-11-14 15:21:53 -0300

    add inquirer

--- aws-upload.js
+++ aws-upload.js
@@ -1,94 +1,85 @@
-import dotenv from 'dotenv';
-import AWS from 'aws-sdk';
-import stream from 'stream';
-import fs from 'fs';
+import dotenv from 'dotenv'
+import AWS from 'aws-sdk'
+import stream from 'stream'
+import fs from 'fs'
 
 dotenv.config()
 
-AWS.config.update({ region: 'us-east-1' });
+AWS.config.update({ region: 'us-east-1' })
 
 const s3 = new AWS.S3({
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
 })
 
 export default class AWSUpload {
+  S3PathToRepo = ''
+  chapter = ''
+  fileName = ''
+  videosLocalPath = ''
 
-    S3PathToRepo = ""
-    chapter = ""
-    fileName = ""
-    videosLocalPath = ""
+  constructor (S3PathToRepo, chapter, fileName, videosLocalPath) {
+    this.S3PathToRepo = S3PathToRepo
+    this.chapter = chapter
+    this.fileName = fileName
+    this.videosLocalPath = videosLocalPath
+  }
 
-    constructor(S3PathToRepo, chapter, fileName, videosLocalPath) {
-        this.S3PathToRepo = S3PathToRepo
-        this.chapter = chapter
-        this.fileName = fileName
-        this.videosLocalPath = videosLocalPath
+  uploadVideos () {
+    const fileStream = new stream.PassThrough()
 
+    const pathToS3 = this.S3PathToRepo + this.chapter + this.fileName
+
+    const uploadParams = {
+      Bucket: process.env.AWS_BUCKET_NAME_UPLOAD,
+      Body: fileStream,
+      Key: pathToS3
     }
+    console.log(`Uploading to ${pathToS3}`)
+    s3.upload(uploadParams).promise()
 
-    uploadVideos() {
+    const readStream = fs.createReadStream(this.videosLocalPath)
 
-        const fileStream = new stream.PassThrough();
+    readStream.pipe(fileStream)
+  }
 
-        const pathToS3 = this.S3PathToRepo + this.chapter + this.fileName
-
-        const uploadParams = {
-            Bucket: process.env.AWS_BUCKET_NAME_UPLOAD,
-            Body: fileStream,
-            Key: pathToS3
-        }
-        console.log(`Uploading to ${pathToS3}`)
-        s3.upload(uploadParams).promise()
-
-        const readStream = fs.createReadStream(this.videosLocalPath);
-
-        readStream.pipe(fileStream)
+  async changePathToPublicRead () {
+    const params = {
+      Bucket: process.env.AWS_BUCKET_NAME_READ,
+      Prefix: this.S3PathToRepo + this.chapter + this.fileName
     }
+    const { Contents } = await s3.listObjectsV2(params).promise()
 
-    async changePathToPublicRead() {
-        const params = {
-            Bucket: process.env.AWS_BUCKET_NAME_READ,
-            Prefix: this.S3PathToRepo + this.chapter + this.fileName
-        }
-        const { Contents, IsTruncated } = await s3.listObjectsV2(params).promise();
+    for (const content of Contents) {
+      console.log(content.Key)
+      await s3.putObjectAcl({
+        Bucket: process.env.AWS_BUCKET_NAME_READ,
+        Key: content.Key,
+        AccessControlPolicy: {
+          Grants: [
+            {
+              Grantee: {
+                Type: 'CanonicalUser',
+                DisplayName: 'wesleywillians',
+                ID: 'a3edb89dc8762b1d543412e1b0999c8b17e8a1e94c3694bf2e35d4b61499419d'
 
-        for (const content of Contents) {
-            console.log(content.Key)
-            await s3.putObjectAcl({
-                Bucket: process.env.AWS_BUCKET_NAME_READ,
-                Key: content.Key,
-                AccessControlPolicy: {
-                    Grants: [
-                        {
-                            Grantee: {
-                                Type: 'CanonicalUser',
-                                DisplayName: 'wesleywillians',
-                                ID: 'a3edb89dc8762b1d543412e1b0999c8b17e8a1e94c3694bf2e35d4b61499419d',
-
-                            },
-                            Permission: 'FULL_CONTROL'
-                        },
-                        {
-                            Grantee: {
-                                Type: "Group",
-                                URI: 'http://acs.amazonaws.com/groups/global/AllUsers',
-                            },
-                            Permission: "READ",
-                        }
-                    ],
-                    Owner: {
-                        DisplayName: 'wesleywillians',
-                        ID: 'a3edb89dc8762b1d543412e1b0999c8b17e8a1e94c3694bf2e35d4b61499419d',
-                    }
-                }
-            })
+              },
+              Permission: 'FULL_CONTROL'
+            },
+            {
+              Grantee: {
+                Type: 'Group',
+                URI: 'http://acs.amazonaws.com/groups/global/AllUsers'
+              },
+              Permission: 'READ'
+            }
+          ],
+          Owner: {
+            DisplayName: 'wesleywillians',
+            ID: 'a3edb89dc8762b1d543412e1b0999c8b17e8a1e94c3694bf2e35d4b61499419d'
+          }
         }
-        if (IsTruncated) {
-            this.params.ContinuationToken = data.NextContinuationToken;
-            console.log("get further list...");
-        }
+      })
     }
+  }
 }
-
-
--- index.js
+++ index.js
@@ -1,115 +1,142 @@
-import dotenv from 'dotenv';
+import dotenv from 'dotenv'
 import { getVideoDurationInSeconds } from 'get-video-duration'
-import path from 'path';
-import fs from 'fs';
+import path from 'path'
+import fs from 'fs'
+import inquirer from 'inquirer'
 import AWSUpload from './aws-upload.js'
 
 dotenv.config()
 
-const REPO = process.env.REPO_JAVA
-const CHAPTER = "teste/"
+let REPO = ''// process.env.REPO_JAVA
+let CHAPTER = 'teste/'
+
+inquirer.prompt([
+  {
+    type: 'list',
+    name: 'Options',
+    message: 'Selecione o repositório da lista abaixo:',
+    choises: ['REPO_TYPESCRIPT', 'REPO_DOTNET', 'REPO_REACT',
+      'REPO_JAVA', 'REPO_PHP', 'REPO_PYTHON', 'REPO_DEPLOY_CLOUDS'
+    ]
+  }
+])
+  .then(answer => {
+    REPO = process.env + answer
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      console.log(error)
+    }
+  })
+
+inquirer.prompt([
+  'Agora digite o número do capítulo'
+])
+  .then(answer => {
+    CHAPTER = answer + '/'
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      console.log(error)
+    }
+  })
 
 class Videos {
+  repo = ''
+  chapter = ''
 
-    repo = ""
-    chapter = ""
+  constructor (repo, chapter) {
+    this.repo = repo
+    this.chapter = chapter
+  }
 
-    constructor(repo, chapter) {
-        this.repo = repo
-        this.chapter = chapter
+  createFileVideosDuration = async () => {
+    this._renameFiles()
+
+    const files = this.getFilesPath(false)
+    let videos = ''
+
+    for (const file of files) {
+      const time = (await getVideoDurationInSeconds(`videos/${file}`))
+      videos += this.repo + this.chapter + file + ' ' + this._formatTime(time) + '\r\n'
     }
+    this._saveInFile(videos)
+    console.log('Arquivo gerado com sucesso')
+  }
 
-    createFileVideosDuration = async () => {
-
-        const files = this.getFilesPath(false)
-        let videos = ""
-
-        this._renameFiles()
-
-        for (const file of files) {
-            const time = (await getVideoDurationInSeconds(`videos/${file}`))
-            videos += this.repo + this.chapter + file + " " + this._formatTime(time) + "\r\n"
-        }
-        this._saveInFile(videos)
-        this._renameFiles()
+  getFilesPath (completePath) {
+    const directoryPath = path.join(path.resolve(), 'videos')
+    const fileNames = fs.readdirSync(directoryPath)
+    if (completePath === false) {
+      return fileNames
     }
-
-    getFilesPath(completePath) {
-        const directoryPath = path.join(path.resolve(), 'videos');
-        const fileNames = fs.readdirSync(directoryPath)
-        if (completePath === false) {
-            return fileNames
-        }
-        if (completePath === true) {
-            const result = []
-            fileNames.forEach(element => {
-                result.push(`videos/${element}`)
-
-            });
-            return result
-        }
+    if (completePath === true) {
+      const result = []
+      fileNames.forEach(element => {
+        result.push(`videos/${element}`)
+      })
+      return result
     }
+  }
 
-    _saveInFile = (videos) => {
-        fs.writeFileSync('videos.txt', videos, function (err) {
-            if (err) return console.log(err);
-            console.log(`Video ${video} added`);
-        })
-    }
+  _saveInFile = (videos) => {
+    fs.writeFileSync('videos.txt', videos, function (err) {
+      if (err) return console.log(err)
+      console.log(`Video ${videos} added`)
+    })
+  }
 
-    _formatTime = (seconds) => {
-        const dateObject = new Date(seconds * 1000)
+  _formatTime = (seconds) => {
+    const dateObject = new Date(seconds * 1000)
 
-        const videoMinutes = dateObject.getUTCMinutes();
+    const videoMinutes = dateObject.getUTCMinutes()
 
-        const videoSeconds = dateObject.getUTCSeconds();
+    const videoSeconds = dateObject.getUTCSeconds()
 
-        return videoMinutes.toString().padStart(2, '0') + ':' +
+    return videoMinutes.toString().padStart(2, '0') + ':' +
             videoSeconds.toString().padStart(2, '0')
+  }
+
+  _filenameNormalized = (file) => {
+    const map = {
+      a: 'á|à|ã',
+      A: 'Á|À|Ã',
+      e: 'é|ê',
+      E: 'É|Ê',
+      i: 'í',
+      I: 'Í',
+      o: 'ó|õ|ô',
+      O: 'Ó|Õ|Ô',
+      u: 'ú',
+      U: 'Ú',
+      c: 'ç',
+      C: 'Ç',
+      '-': ' '
     }
 
-    _filenameNormalized = (file) => {
-        var map = {
-            "a": "á|à|ã",
-            "A": "Á|À|Ã",
-            "e": "é|ê",
-            "E": "É|Ê",
-            "i": "í",
-            "I": "Í",
-            "o": "ó|õ|ô",
-            "O": "Ó|Õ|Ô",
-            "u": "ú",
-            "U": "Ú",
-            "c": "ç",
-            "C": "Ç",
-            "-": " "
-        };
-
-        let str = file
-        for (var i in map) {
-            str = str.replace(new RegExp(map[i], "g"), i);
-        }
-        return str;
+    let str = file
+    for (const i in map) {
+      str = str.replace(new RegExp(map[i], 'g'), i)
     }
+    return str
+  }
 
-    _renameFiles() {
-        this._orderByLastModified();
-        const files = this.getFilesPath(true)
-        files.forEach((file, index) => {
-            fs.renameSync(file, `${this._filenameNormalized(file)}`);
-        });
-    }
+  _renameFiles () {
+    this._orderByLastModified()
+    const files = this.getFilesPath(true)
+    files.forEach((file, index) => {
+      fs.renameSync(file, `${this._filenameNormalized(file)}`)
+    })
+  }
 
-    _orderByLastModified() {
-        const dir = "./"
-        const files = this.getFilesPath(true)
-        files.sort((a, b) => {
-            return fs.statSync(dir + a).mtime.getTime() -
-                fs.statSync(dir + b).mtime.getTime();
-        });
-    }
-
-
+  _orderByLastModified () {
+    const dir = './'
+    const files = this.getFilesPath(true)
+    files.sort((a, b) => {
+      return fs.statSync(dir + a).mtime.getTime() -
+                fs.statSync(dir + b).mtime.getTime()
+    })
+  }
 }
 
 const videos = new Videos(REPO, CHAPTER)
@@ -120,29 +147,53 @@ const fullLocalPath = videos.getFilesPath(true)
 const fileNames = videos.getFilesPath(false)
 
 const uploadVideos = () => {
-    for (let i = 0; i != fullLocalPath.length; i++) {
-        const awsUpload = new AWSUpload(
-            REPO,
-            CHAPTER,
-            fileNames[i],
-            fullLocalPath[i]
-        )
-        awsUpload.uploadVideos()
-    }
+  for (let i = 0; i !== fullLocalPath.length; i++) {
+    const awsUpload = new AWSUpload(
+      REPO,
+      CHAPTER,
+      fileNames[i],
+      fullLocalPath[i]
+    )
+    awsUpload.uploadVideos()
+  }
 }
 
 const changePermission = async () => {
-    for (let i = 0; i != fullLocalPath.length; i++) {
-        const awsUpload = new AWSUpload(
-            REPO,
-            CHAPTER,
-            fileNames[i],
-            fullLocalPath[i]
-        )
-        await awsUpload.changePathToPublicRead()
-    }
+  for (let i = 0; i !== fullLocalPath.length; i++) {
+    const awsUpload = new AWSUpload(
+      REPO,
+      CHAPTER,
+      fileNames[i],
+      fullLocalPath[i]
+    )
+    await awsUpload.changePathToPublicRead()
+  }
 }
 
-// uploadVideos()
+inquirer.prompt([
+  'Deseja fazer o upload agora?'
+])
+  .then(answer => {
+    if (answer === 'y') {
+      uploadVideos()
+    }
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      console.log(error)
+    }
+  })
 
-// await changePermission()
+inquirer.prompt([
+  'Deseja alterar as permissões no S3 agora?'
+])
+  .then(async answer => {
+    if (answer === 'y') {
+      await changePermission()
+    }
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      console.log(error)
+    }
+  })
