767a5c5158aa67d8c60c45e85e94e0284e68448c
Gabriel Carneiro Jr <gacarneirojr@hotmail.com>
2023-08-22 10:39:16 -0300

    add mba modules

--- src/cli-questions.js
+++ src/cli-questions.js
@@ -2,9 +2,21 @@ const questions = [{
   type: 'list',
   name: 'repos',
   message: 'Selecione o repositório da lista abaixo:',
-  choices: ['TYPESCRIPT', 'DOTNET', 'REACT',
-    'JAVA', 'PHP', 'PYTHON', 'DEPLOY_CLOUDS',
-    'GOLANG', 'EDA'
+  choices: [
+    'MBA_clean_hex_arch',
+    'MBA_domain_driven_design',
+    'MBA_fund_arquitetura_de_software',
+    'MBA_solid_design_pattern',
+    'MBA_solution_architecture',
+    'MBA_system_design_and_design_docs',
+    'TYPESCRIPT',
+    'DOTNET',
+    'REACT',
+    'JAVA',
+    'PHP',
+    'PYTHON',
+    'DEPLOY_CLOUDS',
+    'KEYCLOAK'
   ]
 },
 {
@@ -27,15 +39,20 @@ const questions = [{
 ]
 
 const REPOS = {
+  MBA_clean_hex_arch: 'code/mba/clean-hex-arch/',
+  MBA_domain_driven_design: 'code/mba/domain-driven-design/',
+  MBA_fund_arquitetura_de_software: 'code/mba/fund-arquitetura-de-software/',
+  MBA_solid_design_pattern: 'code/mba/solid-design-pattern/',
+  MBA_solution_architecture: 'code/mba/solution-architecture/',
+  MBA_system_design_and_design_docs: 'code/mba/system-design-and-design-docs/',
   TYPESCRIPT: 'code/fullcycle/fc3/microsservico-catalogo-de-videos-com-typescript/',
   DOTNET: 'code/fullcycle/fc3/microsservico-catalogo-de-videos-com-dotnet/',
-  REACT: 'code/fullcycle/fc3/microsservico-administracao-do-catalogo-de-videos-com-React/',
-  JAVA: 'code/fullcycle/fc3/microsservico-catalogo-de-videos-com-java-new/',
-  PHP: 'code/fullcycle/fc3/microsservico-catalogo-de-videos-com-php/',
+  REACT: 'code/fullcycle/fc3/projeto-pratico-codeflix-react/',
+  JAVA: 'code/fullcycle/fc3/microsservico-api-do-catalogo-com-java/',
+  PHP: 'code/fullcycle/fc3/microsservico-api-do-catalogo-com-php/',
   PYTHON: 'code/fullcycle/fc3/microsservico-catalogo-de-videos-com-python/',
   DEPLOY_CLOUDS: 'code/fullcycle/Deploy-das-Cloud-Providers/',
-  GOLANG: 'code/go/',
-  EDA: 'code/fullcycle/fc3/EDA/'
+  KEYCLOAK: 'code/fullcycle/autenticacao-com-keycloack-new/'
 }
 
 export { questions, REPOS }
