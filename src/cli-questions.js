fd4d81bea2d87f4ec5ca2ff72d46032b4eb3adf9
Lucian Tavares <lucian.sistemas.sc@gmail.com>
2024-05-10 10:24:28 -0300

    modificação nos repos de vídeo.

--- src/cli-questions.js
+++ src/cli-questions.js
@@ -5,12 +5,15 @@ const questions = [{
   choices: [
     'DevOpsPro',
     'DevOpsProBonus',
+    'DevOpsProBonusWorkshops',
     'MBA_marketing_pessoal',
     'TYPESCRIPT',
     'DOTNET',
     'REACT',
     'JAVA',
+    'JAVA_subscription_service',
     'PHP',
+    'PHP_subscription_service',
     'PYTHON',
     'DEPLOY_CLOUDS',
     'KEYCLOAK',
@@ -22,7 +25,10 @@ const questions = [{
     'MBA_solution_architecture',
     'MBA_system_design_and_design_docs',
     'MBA_lideranca',
-    'GOEXPERT'
+    'MBA_apache_kafka',
+    'MBA_banco_de_dados',
+    'GOEXPERT',
+    'GOEXPERT_labs'
   ]
 },
 {
@@ -47,12 +53,15 @@ const questions = [{
 const REPOS = {
   DevOpsPro: 'devopspro/18-kubernetes-aplicacoes/',
   DevOpsProBonus: 'devopspro/bonus/encontros_ao_vivo/',
+  DevOpsProBonusWorkshops: 'devopspro/bonus/workshops/',
   MBA_marketing_pessoal: 'code/mba/marketing_pessoal/',
   TYPESCRIPT: 'code/fullcycle/fc3/microsservico-catalogo-de-videos-com-typescript-v2/',
   DOTNET: 'code/fullcycle/fc3/microsservico-api-do-catalogo-com-dotnet/',
   REACT: 'code/fullcycle/fc3/projeto-pratico-codeflix-react/',
   JAVA: 'code/fullcycle/fc3/microsservico-api-do-catalogo-com-java/',
+  JAVA_subscription_service: 'code/fullcycle/fc3/microsservico-assinatura-com-java/',
   PHP: 'code/fullcycle/fc3/microsservico-api-do-catalogo-com-php/',
+  PHP_subscription_service: 'code/fullcycle/fc3/microsservico-assinatura-com-php/',
   PYTHON: 'code/fullcycle/fc3/microsservico-catalogo-de-videos-com-python-v2/',
   DEPLOY_CLOUDS: 'code/fullcycle/Deploy-das-Cloud-Providers/',
   KEYCLOAK: 'code/fullcycle/autenticacao-com-keycloack-new/',
@@ -64,7 +73,10 @@ const REPOS = {
   MBA_solution_architecture: 'code/mba/solution-architecture/',
   MBA_system_design_and_design_docs: 'code/mba/system-design-and-design-docs/',
   MBA_lideranca: 'code/mba/lideranca/',
-  GOEXPERT: 'code/go/'
+  MBA_apache_kafka: 'code/mba/apache_kafka/',
+  MBA_banco_de_dados: 'code/mba/banco_de_dados/',
+  GOEXPERT: 'code/go/',
+  GOEXPERT_labs: 'code/go/labs/'
 }
 
 export { questions, REPOS }
